46bb33cf020008920c1008ce17a76a9cd65833ec
brivu <emailbrian.vu@gmail.com>
2021-01-23 16:29:07 -1000

    Completed CRUD operations

--- model/index.js
+++ model/index.js
@@ -20,5 +20,35 @@ function getRating(shoeId) {
   });
 }
 
+function createReview(reviewData) {
+  return Review.create({
+    name: reviewData.name,
+    headline: reviewData.headline,
+    review: reviewData.review,
+    rating: reviewData.rating,
+    fit_feedback: reviewData.fit_feedback,
+    shoe_id: reviewData.shoe_id
+  });
+}
+
+function updateReview(reviewId, reviewData){
+  return Review.update(reviewData, {
+    where: {
+      id: reviewId
+    }
+  });
+}
+
+let removeReview = (reviewId) => {
+  return Review.destroy({
+    where: {
+        id: reviewId
+    }
+  });
+}
+
 module.exports.getReviews = getReviews;
 module.exports.getRating = getRating;
+module.exports.createReview = createReview;
+module.exports.updateReview = updateReview;
+module.exports.removeReview = removeReview;
--- server/index.js
+++ server/index.js
@@ -4,6 +4,9 @@ const app = express();
 const PORT = 3003;
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 
 app.get('/shoes/:shoeId/reviews/:count', (req, res) => {
   let { shoeId, count } = req.params;
@@ -28,5 +31,40 @@ app.get('/shoes/:shoeId/rating', (req, res) => {
     res.end();
   });
 });
+
+app.post('/reviews/create', (req, res) => {
+  controller.createReview(req.body)
+  .then((data) => {
+    res.status(200).send('Review created successfully');
+  })
+  .catch(err => {
+    console.error(err);
+    res.status(404).end('Error creating Review');
+  });
+})
+
+app.put('/reviews/:reviewId/update', (req, res) => {
+  let reviewId = req.params.reviewId;
+  controller.updateReview(reviewId, req.body)
+  .then((data) => {
+    res.status(200).send('Review updated successfully');
+  })
+  .catch(err => {
+    console.error(err);
+    res.status(404).end('Error updating Review');
+  });
+})
+
+app.delete('/reviews/:reviewId/remove', (req, res) => {
+  let reviewId = req.params.reviewId;
+  controller.removeReview(reviewId)
+  .then((data) => {
+    res.status(200).send('Review removed successfully');
+  })
+  .catch(err => {
+    console.error(err);
+    res.status(404).end('Error removing Review');
+  });
+})
 
 module.exports = app;
